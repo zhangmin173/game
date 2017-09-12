2185e707742d2b0dfdc6608b7e005523619abf8f
min zhang <min zhang>
2017-09-12 22:43:59 +0800

    update

--- tetris/js/tetris.js
+++ tetris/js/tetris.js
@@ -4,9 +4,9 @@
 class Square {
 
 	constructor() {
-		this.block = [
-			[2,2,2,2],
-			[0,0,0,0],
+		this.data = [
+			[2,2,2,0],
+			[0,0,2,0],
 			[0,0,0,0],
 			[0,0,0,0]
 		]
@@ -28,7 +28,7 @@ class Square {
 	left() {
 		this.origin.y--;
 	}
-	
+
 }
 /**
  * 俄罗斯方块儿基础类
@@ -41,8 +41,8 @@ class Tetris {
 		this.color = ['none','done','active']
 		this.n = 10
 		// 游戏区
-		this.ele = ele
-		this.area = [
+		this.gameBox = ele
+		this.data = [
 			[0,0,0,0,0,0,0,0,0,0],
 			[0,0,0,0,0,0,0,0,0,0],
 			[0,0,0,0,0,0,0,0,0,0],
@@ -64,9 +64,13 @@ class Tetris {
 			[0,0,0,0,0,0,0,0,0,0],
 			[0,0,0,0,0,0,0,0,0,0]
 		]
+		this.gameDoms = []
 		// 预览区
-		this.preview = opts.preview
+		this.nextBox = opts.preview
+		this.nextDoms = []
+		// 当前方块儿
 		this.cur = null
+		// 下一个方块儿
 		this.next = null
 	}
 
@@ -82,11 +86,17 @@ class Tetris {
 		this.cur = new Square(),
 		this.next = new Square()
 
-		this.updateArea(this.cur)
-		this.updateDom(this.ele,this.area)
-		this.updateDom(this.preview,this.next.block)
+		this.initDom(this.gameBox,this.data,this.gameDoms)
+		this.initDom(this.nextBox,this.next.data,this.nextDoms)
 
-		this.handle()
+		//this.updateDom(this.data,this.gameDoms)
+
+
+		// this.updateArea(this.cur)
+		// this.updateDom(this.gameBox,this.data)
+		// this.updateDom(this.nextBox,this.next.data)
+
+		// this.handle()
 	}
 	// 事件绑定
 	handle() {
@@ -117,53 +127,57 @@ class Tetris {
 	}
 	// 左
 	left() {
-		this.cur.left();
+		this.cur.left()
 		this.updateArea(this.cur)
-		this.updateDom(this.ele,this.area)
+		this.updateDom(this.gameBox,this.data)
 	}
 	// 上
 	// 右
 	right() {
-		this.cur.right();
+		this.cur.right()
 		this.updateArea(this.cur)
-		this.updateDom(this.ele,this.area)
+		this.updateDom(this.gameBox,this.data)
 	}
 	// 下
 	down() {
 		if (this.canDown()) {
-			this.cur.down();
+			this.cur.down()
 			this.updateArea(this.cur)
 			this.bindArea()
-			this.updateDom(this.ele,this.area)
+			this.updateDom(this.gameBox,this.data)
+		} else {
+			this.cur = this.next
+			this.next = new Square()
 		}
 	}
 	canDown() {
 		if (this.cur.origin.x > (this.n *2 - 2)) {
-			this.cur.over = true;
 			return false
+		}
+		if (this.cur.origin.x > (this.n *2 - 3)) {
+			this.cur.over = true;
 		}
 		return true
 	}
+
 	// 更新游戏显示区数据
 	updateArea(cur) {
 		let _this = this
-		this.area.forEach( function(element, i) {
+		this.data.forEach( function(element, i) {
 			element.forEach( function(ele, j) {
-				if (_this.area[i][j] == 2) {
-					_this.area[i][j] = 0
+				if (_this.data[i][j] == 2) {
+					_this.data[i][j] = 0
 				}
-			    
 			});
 		});
-		cur.block.forEach( function(element, i) {
+		cur.data.forEach( function(element, i) {
 			element.forEach( function(ele, j) {
 				let x = i + cur.origin.x,
 					y = j + cur.origin.y
-
-				if (cur.block[i][j] == 2) {
-					_this.area[x][y] = cur.block[i][j]
+				if (cur.data[i][j] == 2) {
+					_this.data[x][y] = cur.data[i][j]
 				}
-			    
+
 			});
 		});
 	}
@@ -173,31 +187,41 @@ class Tetris {
 			return
 		}
 		let _this = this
-		this.area.forEach( function(element, i) {
+		this.data.forEach( function(element, i) {
 			element.forEach( function(ele, j) {
-				if (_this.area[i][j] == 2) {
-					_this.area[i][j] = 1
+				if (_this.data[i][j] == 2) {
+					_this.data[i][j] = 1
 				}
 			});
 		});
 	}
-	// 更新dom
-	updateDom(container,data) {
-		let html = ''
-		for (let i = 0,len = data.length; i < len; i++) {
-			for (let j = 0,l = data[i].length; j < l; j++) {
-				let div = document.createElement('div'),
+	// 初始化dom元素
+	initDom(container,data,divs) {
+		let _this = this
+
+		data.forEach( function(element, i) {
+			let div = []
+			element.forEach( function(ele, j) {
+				let parent = document.createElement('div'),
 					node = document.createElement('span')
 
-				node.style.width = this.w + 'px'
-				node.style.height = this.w + 'px'
-				node.className = this.color[data[i][j]]
-
-				div.appendChild(node)
-				html += div.innerHTML
-			}
-		}
-		container.innerHTML = html
+				node.style.width = _this.w + 'px'
+				node.style.height = _this.w + 'px'
+				node.className = _this.color[0]
+				div.push(node)
+				container.appendChild(node)
+			});
+			divs.push(div)
+		});
+	}
+	// 更新dom
+	updateDom(data,divs) {
+		let _this = this
+		data.forEach( function(element, i) {
+			element.forEach( function(ele, j) {
+				divs[i][j].className = _this.color[data[i][j]]
+			});
+		});
 	}
 }
 
